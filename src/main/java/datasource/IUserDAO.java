16ec198b66662fe8f8eb4aa89c2a079ab52407f2
RickGZ <rickgz66@gmail.com>
2017-10-12 11:24:57 +0200

    adjusted interface to boolean

--- src/main/java/datasource/IUserDAO.java
+++ src/main/java/datasource/IUserDAO.java
@@ -1,7 +1,5 @@
 package datasource;
 
-import domain.User;
-
 public interface IUserDAO {
-    User findUserByUsernameAndPassword(String username, String password);
+    boolean findUserByUsernameAndPassword(String username, String password);
 }
---
+++ src/main/java/datasource/UserDAO.java
@@ -0,0 +1,10 @@
+package datasource;
+
+import domain.User;
+
+public class UserDAO implements IUserDAO {
+
+    public boolean findUserByUsernameAndPassword(String username, String password) {
+        return true;
+    }
+}
