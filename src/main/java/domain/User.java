c4a086b77046b8f9f1ca5162a638d64cb01b2eb2
RickGZ <rickgz66@gmail.com>
2017-10-12 12:06:25 +0200

    added returnable json user to test login

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -1,5 +1,13 @@
 package domain;
 
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/login")
 public class User {
     private String username;
     private String password;
@@ -10,6 +18,16 @@ public class User {
 
     public String getUsername() {
         return username;
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonObject setUsernameAndPassword() {
+        System.out.println("hoi");
+
+        JsonObject jo = Json.createObjectBuilder().add("token", "1234-1234-1234").add("user", "Rick Zweers").build();
+
+        return jo;
     }
 
     public void setUsername(String username) {
