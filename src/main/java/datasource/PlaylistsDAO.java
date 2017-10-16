42f862b1b9cf00ea1e785304f0c9e5a26523549f
RickGZ <rickgz66@gmail.com>
2017-10-16 17:18:18 +0200

    started work on playlistDAO

--- src/main/java/datasource/PlaylistsDAO.java
+++ src/main/java/datasource/PlaylistsDAO.java
@@ -1,8 +1,38 @@
 package datasource;
 
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class PlaylistsDAO extends Database {
 
-    public void findAllPlaylists() {
+    public JsonObject findAllPlaylists() {
+        PreparedStatement statement = null;
+        ResultSet result = null;
+
+        JsonArray playlistArray = Json.createArrayBuilder();
+
+        try {
+            statement = connection.prepareStatement("SELECT * from Playlist");
+
+            System.out.println(statement);
+            result = statement.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+            result.beforeFirst();
+            result.next();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
