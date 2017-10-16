d166c1fb8673936ddf07b626d4b690d69b2bc623
RickGZ <rickgz66@gmail.com>
2017-10-16 12:53:39 +0200

    added a hardcoded return value for playlists

--- src/main/java/services/Playlists.java
+++ src/main/java/services/Playlists.java
@@ -1,5 +1,9 @@
 package services;
 
+import domain.Track;
+
+import javax.json.Json;
+import javax.json.JsonArray;
 import javax.json.JsonObject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -11,9 +15,30 @@ import javax.ws.rs.core.MediaType;
 public class Playlists {
 
     @GET
-    @Consumes(MediaType.APPLICATION_JSON);
-    @Produces(MediaType.APPLICATION_JSON);
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     public JsonObject loadPlaylists() {
 
+        System.out.println("playlists laden");
+
+
+        JsonObject track = Json.createObjectBuilder().add("id", 2).add("title", "testtitel").
+                add("performer", "testperformer").add("duration", 100).add("album", "testalbum").
+                add("playcount", 0).add("publicationDate", 10000000).add("description", "kekkerino").
+                add("offlineAvailable", true).build();
+
+        JsonArray tracks = Json.createArrayBuilder().add(track).build();
+
+
+        JsonObject playlist = Json.createObjectBuilder().add("id", 1).add("name", "test").
+                add("owner", true).add("tracks", tracks).build();
+
+        JsonArray playlistArray = Json.createArrayBuilder().add(playlist).build();
+
+        JsonObject playlists = Json.createObjectBuilder().add("playlists", playlistArray).
+                add("length", 100).build();
+
+
+        return playlists;
     }
 }
