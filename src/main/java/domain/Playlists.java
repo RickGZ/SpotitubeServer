bb5b75f2a8a25d20a8533080a4b8399240909e71
RickGZ <rickgz66@gmail.com>
2017-10-16 11:54:48 +0200

    created Playlists pojo

---
+++ src/main/java/domain/Playlists.java
@@ -0,0 +1,42 @@
+package domain;
+
+import java.util.ArrayList;
+
+public class Playlists {
+    private int id;
+    private String name;
+    private boolean owner;
+    private ArrayList<Track> tracks;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isOwner() {
+        return owner;
+    }
+
+    public void setOwner(boolean owner) {
+        this.owner = owner;
+    }
+
+    public ArrayList<Track> getTracks() {
+        return tracks;
+    }
+
+    public void setTracks(ArrayList<Track> tracks) {
+        this.tracks = tracks;
+    }
+}
