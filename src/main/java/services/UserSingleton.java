533a7e7fe07ba976ee1dde23ca9a58ab9b9b8200
RickGZ <rickgz66@gmail.com>
2017-11-06 23:39:12 +0100

    added singleton comment

--- src/main/java/services/UserSingleton.java
+++ src/main/java/services/UserSingleton.java
@@ -1,6 +1,7 @@
 package services;
 
 public class UserSingleton {
+    //In deze singleton wordt de gebruikersnaam opgeslagen.
     private static UserSingleton ourInstance = new UserSingleton();
 
     private static String username;
